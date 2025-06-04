617079802e57344595d33b8e307aa3f211f67248
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-06-04 14:21:10 +0600

    Done

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const urlRoutes = require('./routes/urlRoutes');
+const cors = require('cors');
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: ['https://link-trim-iota.vercel.app'], // your frontend URL
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
