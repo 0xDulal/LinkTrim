fc33ec95720b924561cb06d6f137e59d975a9635
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-06-04 14:19:41 +0600

    Final Fix

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/', urlRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
