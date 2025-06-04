587e6c9a3d8b093bf83ceb55f72668a0314ae2d7
Dulal Shikdar <91406055+0xDulal@users.noreply.github.com>
2025-06-04 14:24:49 +0600

    Fix Env

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/shorten', createShortUrl);
 router.get('/:shortId', redirectToOriginalUrl);
 router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to LinkTrim URL Shortener API' });
+    res.status(200).json({ message: 'Welcome to LinkTrim URL Shortener API <3' });
 });
 
 module.exports = router;
